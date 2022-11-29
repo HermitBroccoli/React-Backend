ce0b0a4de6c572a07256b10cb58eb625b2569ade
broccoli <zeratukin@gmail.com>
2022-11-29 22:17:56 +1000

    edit

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const expressHandlebars = require('express-handlebars');
+const expressHandlebars = require('express-handlebars').engine;
 
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+const fortunes = [
+    "Победи свои страхи или они победят тебя.",
+    "Рекам нужны истоки.",
+    "Не бойся неведомого.",
+    "Тебя ждёт приятный сюрприз",
+    "Будь проще везде, где только можно"
+]
+
+app.use(express.static(__dirname + '/public'))
 
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main'
@@ -13,7 +23,11 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => res.render('home'))
 
-app.get('/about', (req, res) => res.render('about'))
+app.get('/about', (req, res) => {
+    const randomFortunes = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.status(200)
+    res.render('about', {fortune: randomFortunes})
+})
 
 app.use((res, req)=>{
     res.status(404);
